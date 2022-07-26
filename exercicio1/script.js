53dbdaa3c2c2f3cbf5f9168871b700e089a490e6
ThiagoMachadoL <thiagojkp123@gmail.com>
2022-07-25 23:10:27 -0300

    exercicio-fixacao-9

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const nome = prompt("Qual o seu nome? ")
+const comida1 = prompt("Qual a sua comida preferida? ")
+const comida2 = prompt("Qual a sua segunda comida preferida? ")
+const comida3 = prompt("Qual a sua terceira comida preferida? ")
+
+console.log("Estas são as comidas preferidas do " +nome+":\n"+comida1+"\n"+comida2+"\n"+comida3)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,16 @@
-//Crie a const para a frase aqui
+/* a) */
+const frase= "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair";
+console.log(frase);
+
+/* b) */
+const trocaDeCor = frase.replace("verde", "rosa").replace("azul","laranja");
+console.log(trocaDeCor);
+
+/* c) */
+console.log(trocaDeCor.includes("verde"));
+console.log(trocaDeCor.includes("laranja"));
+
+/* Extra */ 
+const frase1 = "Jorge tem uma casa verde e com portão azul, com os dizeres:\"BOAS VINDAS,"
+const frase2 = " mas não deixe o gato sair";
+console.log(frase1+frase2.toUpperCase()); 
